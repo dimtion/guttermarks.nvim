25d2f96dc741047737c01df37eb385288b752a46
Loïc Carr <dev@carr.bzh>
2026-02-14 13:37:31 -0800

    fix: Remove duplicate " in special marks

--- lua/guttermarks/config.lua
+++ lua/guttermarks/config.lua
@@ -33,7 +33,7 @@ return {
   special_mark = {
     enabled = false,
     sign = nil,
-    marks = { "'", "^", ".", "[", "]", "<", ">", '"', "`", '"', "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" },
+    marks = { "'", "^", ".", "[", "]", "<", ">", '"', "`", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" },
     highlight_group = "GutterMarksSpecial",
     priority = 10,
   },
